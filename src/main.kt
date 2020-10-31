f9d2970275eb3b72b5ecf9194293874101604a48
ja <you@example.com>
2020-10-31 13:36:20 +0100

    update

--- src/main.kt
+++ src/main.kt
@@ -1,24 +1,29 @@
+import java.lang.NullPointerException
+import java.lang.NumberFormatException
+
 fun main() {
-    var str:String? = "dawid"
+    var str:String? = "Przemysław"
+
     println("Długość napisu: ${str?.length}")
     println("Długość napisu: ${str!!.length}")
 
+    if(true)
+        str = null
 
-   if(true)
-    str = null
     println("Długość napisu: ${str?.length?:-1}")
 
     try {
         println("Długość napisu: ${str!!.length}")
     }catch (e:NullPointerException){
-        println("wyjątek")
+        println("Przechwycono wyjątek: Null Pointer Exception")
     }
 
     var length = 0;
 
     if(str==null){
         length = 0
-    }else length = -1
+    }else
+        length = -1
 
     var length1 = str?.length?:-1
 
@@ -26,7 +31,26 @@ fun main() {
         println(it)
     }
 
+    example1()
+}
 
+fun example1(){
+    var name:String? = "Przemysław"
 
+    name?.let{
+        println("Wartość wynosi: ${it}")
+    }.also{
+        println("To cześć pierwszego bloku also")
+    }
 
+    if(true)
+        name = null  
+    else
+        name = "Przemysław 1"
+
+    name?.let{
+        println("Wartość wynosi: ${it}")
+    }.also {
+        println("To część bloku also")
+    }
 }
